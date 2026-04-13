ece1924833c697087349409dd3f56aa16aa7caba
dapicarte <da.picarte@duocuc.cl>
2026-04-13 19:05:06 -0400

    repo ok

---
+++ src/main/java/com/caso4/practice4Dani/repository/PersonaRepository.java
@@ -0,0 +1,23 @@
+package com.caso4.practice4Dani.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.caso4.practice4Dani.model.Persona;
+
+public class PersonaRepository {
+    private List<Persona> listaPersonas=new ArrayList<>();
+
+    public Persona agregar(Persona persona){
+        listaPersonas.add(persona);
+        return persona;
+    }
+
+    public List<Persona> listar(){
+        return listaPersonas;
+    }
+
+    public boolean eliminar(int id){
+        return listaPersonas.removeIf(b -> b.getId()==id);
+    }
+}
